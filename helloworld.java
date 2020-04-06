8d3390ac929fa4dc695ae0a8757c1f16b987d0fd
San U <46236201+sullah894@users.noreply.github.com>
2020-04-06 16:47:47 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
 	public static void main (String [] args) {
-	System.out.println("Hello, World", "it is working from GIT now");
+	System.out.println("Hello, World", "it is working from GIT now-- it is anonther day with webhuk");
 	}
 }
